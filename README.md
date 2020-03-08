adc0d22fd1de094e7ee185d2e7dffb48d17e465b
yechan96 <yechanlee96@gmail.com>
2020-03-07 21:41:07 -0800

    done

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# bootcamp_hw9_RepoMaker
+# Readme Maker
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/yechan96/bootcamp_hw9_ReadMeMaker)
+## Description
+
+Helps users make readme
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependecies, run the following command:
+
+```
+npm install
+```
+
+## Usage
+
+Nothing Much
+
+## License
+
+MIT
+
+## Contributing
+
+Ask Owner
+
+## Tests
+
+npm run test
+
+## Questions
+
+<img src="https://avatars2.githubusercontent.com/u/26846530?v=4" alt="avatar" style="border-radius: 16px" width="30">
+
+If you have any questions about the repo, open an issue or contact [yechan96](https://api.github.com/users/yechan96) directly at null.
+
---
+++ index.js
@@ -0,0 +1,134 @@
+const inquirer = require('inquirer');
+const GitHub = require('github-api');
+const fs = require('fs');
+const gh = new GitHub();
+const questions =[
+    {
+        type:'input',
+        name:'user',
+        message: 'What is your GitHub username?'
+
+    },
+    {
+        type:'input',
+        name:'projectname',
+        message: 'What is your project\'s name?'
+
+    },
+    {
+        type:'input',
+        name:'description',
+        message: 'Please write a short description of your project:'
+
+    },
+    {
+        type:'input',
+        name:'license',
+        message: 'What kind of license should your project have?',
+        choices: ["MIT","APACHE 2.0","GPL 3.0","BSD 3","None"]
+
+    },
+    {
+        type:'input',
+        name:'dependencies',
+        message: 'What command should be run to install dependencies?'
+
+    },
+    {
+        type:'input',
+        name:'tests',
+        message: 'What command should be run tests?'
+
+    },
+    {
+        type:'input',
+        name:'using',
+        message: 'What does the user need to know about using the repo?'
+
+    },
+    {
+        type:'input',
+        name:'contribute',
+        message: 'What does the user need to know about contributing to the repo?'
+
+    }
+];
+const retreiveUser = (user)=>{
+    return gh.getUser(user).getProfile();
+}
+
+async function main(){
+    const input = await inquirer.prompt(questions);
+    const user = await retreiveUser(input.user);
+    const write = await readMeGen(input,user.data);
+}
+
+main();
+
+
+//the whole thing
+
+
+function readMeGen (input, user){
+    var text =
+`# ${input.projectname}
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/yechan96/bootcamp_hw9_ReadMeMaker)
+## Description
+
+${input.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependecies, run the following command:
+
+\`\`\`
+${input.dependencies}
+\`\`\`
+
+## Usage
+
+${input.using}
+
+## License
+
+${input.license}
+
+## Contributing
+
+${input.contribute}
+
+## Tests
+
+\`\`\`
+${input.tests}
+\`\`\`
+
+## Questions
+
+<img src="${user.avatar_url}" alt="avatar" style="border-radius: 16px" width="30">
+
+If you have any questions about the repo, open an issue or contact [${user.login}](${user.url}) directly at null.
+
+`;
+
+    fs.writeFile('README.md',text,(err)=>{
+        if (err) throw err;
+        console.log("Done!");
+    });
+    return Promise.resolve();
+}
+
